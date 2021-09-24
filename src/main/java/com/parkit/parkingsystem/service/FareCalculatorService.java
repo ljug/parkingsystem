b9a7058953508cf97d18948d3034532db6d68b32
Pascal Fares <pascal.fares@cofares.net>
2021-09-24 07:52:45 +0300

    Une idée de correction

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,5 +1,7 @@
 package com.parkit.parkingsystem.service;
 
+import java.util.Date;
+
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
@@ -10,12 +12,19 @@ public class FareCalculatorService {
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        Date inD = ticket.getInTime();
+        Date outD = ticket.getOutTime();
+        long diff = outD.getTime() - inD.getTime();
+        
+        
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
-
+       
+        double duration;
+        int d_minutes = (int) (diff / (1000*60));
+        if (d_minutes < 15) duration = 0;
+        else duration = d_minutes / 60.;
+        System.out.println("Duration " + duration);       
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
